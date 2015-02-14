d90b85e13f1499edd7ff9761d8e0cf141c6c513b
Paul Richards <paul@richards19.com>
2015-02-14 10:48:33 +0000

    Add less building support

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,3 @@
-
 module.exports = function(grunt) {
 
     // Project configuration.
@@ -7,7 +6,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         requirejs: {
-            compile: {
+            build: {
                 options: {
                     baseUrl: "public/js",
                     mainConfigFile: "public/js/main.js",
@@ -18,11 +17,53 @@ module.exports = function(grunt) {
             }
         },
 
-
         uglify: {
-            my_target: {
+            build: {
                 files: {
                     'build/optimized.min.js': ['build/optimized.js']
+                }
+            }
+        },
+
+        less: {
+            build: {
+                options: {
+                    paths: ["public/css"]
+                },
+                files: {
+                    "build/styles.css": "public/css/styles.less"
+                }
+            },
+            dev: {
+                options: {
+                    paths: ["public/css"]
+                },
+                files: {
+                    "public/css/styles.css": "public/css/styles.less"
+                }
+            }
+
+        },
+
+        cssmin: {
+            build: {
+                options: {
+
+                },
+                target: {
+                    files: {
+                        'build/styles.min.css': [ 'public/css/bootstrap.min.css', 'build/styles.css']
+                    }
+                }
+            }
+        },
+
+        watch: {
+            dev: {
+                files: ['public/css/*.less'], // which files to watch
+                tasks: ['less:dev'],
+                options: {
+                    nospawn: true
                 }
             }
         }
@@ -32,8 +73,12 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Default task(s).
-    grunt.registerTask('default', ['requirejs','uglify']);
+    grunt.registerTask('default', ['requirejs:build','uglify:build','less:build','cssmin:build']);
+    grunt.registerTask('buildless', ['watch:dev']);
 
 };
